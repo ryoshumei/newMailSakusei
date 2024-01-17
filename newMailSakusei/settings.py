102daa3db85231c31a7382835e4866fa2fd5d5c1
daydreamer <s419922572@gmail.com>
2024-01-17 17:23:04 +0900

    Fix static files configuration

--- newMailSakusei/settings.py
+++ newMailSakusei/settings.py
@@ -21,7 +21,7 @@ OPENAI_KEY = os.getenv('OPENAI_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 # Default primary key field type
