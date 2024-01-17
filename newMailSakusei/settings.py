54ab7c20320cbd3c7ea6eaa14936ade3d67bf222
daydreamer <s419922572@gmail.com>
2024-01-17 23:46:47 +0900

    Fix static files configuration, requirements.txt

--- newMailSakusei/settings.py
+++ newMailSakusei/settings.py
@@ -133,3 +133,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MIDDLEWARE_CLASSES = (
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
