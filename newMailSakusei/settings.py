328a5086dd354d109f95d1eb83766c8f1a21a450
daydreamer <s419922572@gmail.com>
2024-04-10 23:17:02 +0900

    added setting for heroku

--- newMailSakusei/settings.py
+++ newMailSakusei/settings.py
@@ -137,8 +137,8 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # for heroku
-# MIDDLEWARE = [
-#     'whitenoise.middleware.WhiteNoiseMiddleware',
-# ]
-#
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
